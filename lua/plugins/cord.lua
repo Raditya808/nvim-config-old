a7a241d01cd6fa7569cbda1fdb73388b892543bb
Raditya808 <mraditradit808@gmail.com>
2025-11-19 18:35:18 +0700

    refactor discord update with line

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,17 +1,41 @@
--- ~/.config/nvim/lua/plugins/cord.lua
+-- discord plugins c:\users\username\appdata\local\nvim\plugins\cord.lua
 return {
   "vyfor/cord.nvim",
   lazy = false,
   config = function()
-    require("cord").setup({
-      -- Ubah dari 'debug' ke 'warn' atau 'error'
+    require('cord').setup({
       log_level = "warn",
-      -- ... konfigurasi lainnya
       text = {
-        title = "Cord",
-        subtitle = "Neovim config",
+        title = "Neovim",
+        subtitle = function()
+          local ft = vim.bo.filetype
+          local bufname = vim.fn.expand("%:t")
+          if ft == "neo-tree" then
+            return "Browsing Files"
+          elseif ft == "alpha" or ft == "dashboard" then
+            return "Welcome Screen"
+          elseif bufname == "" then
+            return "Ready for action!"
+          else
+            local line = vim.fn.line(".")
+            local lines = vim.fn.line("$")
+            return string.format("%s | Line %d of %d", bufname, line, lines)
+          end
+        end,
       },
+      assets = {
+        large_image = "nvim_logo",
+        large_text = "Neovim Editor",
+      },
+    })
+
+    -- Event agar presence tetap update ketika buffer berubah/aktif
+    vim.api.nvim_create_autocmd({
+      "BufEnter", "BufWinEnter", "BufLeave", "CursorMoved", "WinEnter", "WinLeave", "FileType"
+    }, {
+      callback = function()
+       
+      end,
     })
   end,
 }
-
